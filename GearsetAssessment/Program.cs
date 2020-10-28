5ecd4c1652aa73850ebcd330914e5d38271879ad
Ali <parlakciali@gmail.com>
2020-10-28 20:34:49 +0300

    Implement main functionality

--- GearsetAssessment/Program.cs
+++ GearsetAssessment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace GearsetAssessment
 {
@@ -6,7 +8,62 @@ namespace GearsetAssessment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string dest = "C:\\Users\\Ali\\Desktop\\pdf.pdf";
+            string inputPath = "C:\\Users\\Ali\\Desktop\\input.txt";
+
+            using FileStream fileStream = new FileStream(inputPath, FileMode.Open, FileAccess.Read);
+            using StreamReader streamReader = new StreamReader(fileStream);
+
+            string line = String.Empty;
+
+            using (PdfBuilder pdfBuilder = new PdfBuilder(dest))
+            {
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (line.StartsWith(".paragraph"))
+                    {
+                        pdfBuilder.Paragraph();
+                    }
+                    if (line.StartsWith(".normal"))
+                    {
+                        pdfBuilder.Normal();
+                    }
+                    if (line.StartsWith(".large"))
+                    {
+                        pdfBuilder.Large();
+                    }
+                    if (line.StartsWith(".regular"))
+                    {
+                        pdfBuilder.Regular();
+                    }
+                    if (line.StartsWith(".bold"))
+                    {
+                        pdfBuilder.Bold();
+                    }
+                    if (line.StartsWith(".italic"))
+                    {
+                        pdfBuilder.Italic();
+                    }
+                    if (line.StartsWith(".fill"))
+                    {
+                        pdfBuilder.Justify();
+                    }
+                    if (line.StartsWith(".nofill"))
+                    {
+                        pdfBuilder.Left();
+                    }
+                    if (line.StartsWith(".indent"))
+                    {
+                        var indent = line.Split(" ")[1];
+                        pdfBuilder.Indent(Int32.Parse(indent));
+                    }
+                    if (!line.StartsWith("."))
+                    {
+                        pdfBuilder.AddText(" ");
+                        pdfBuilder.AddText(line);
+                    }
+                }
+            }
         }
     }
 }
